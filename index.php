285095dd05097e960d20929c1edc825976f191f8
ramity <lcb005@uark.edu>
2016-05-31 11:55:50 -0500

    init commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+	<head>
+		<link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+		<link href='https://ramity.com/apps/analytics_2/css/dashboard.css' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<div id="top">
+			<div id="topinr">
+				<div id="toplogo">R/Analytics</div>
+				<div id="topmenu">
+					<a class="topmenu">Login</a>
+					<a class="topmenu">Register</a>
+				</div>
+			</div>
+		</div>
+		<div id="content">
+			<div id="contentinr">
+				<div id="sidebar">
+					<a id="sidebarname" href="">Project Ramity</a>
+					<a class="sidebaritem" href="">Audience</a>
+					<a class="sidebaritem" href="">Dashboard</a>
+					<a class="sidebaritem" href="">Demographics</a>
+					<a class="sidebaritem" href="">Geographics</a>
+					<a class="sidebaritem" href="">Goals</a>
+					<a class="sidebaritem" href="">Settings</a>
+				</div>
+				<div id="bar">
+					
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
